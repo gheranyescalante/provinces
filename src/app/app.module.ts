01cff4534e5da35a4cdc76feaa46bd5b53d62a60
gheranyescalante <gheran.escalante@newhorizons.edu.do>
2021-12-02 20:24:16 +0000

    primer template

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BottomComponent } from './bottom/bottom.component';
+import { TopComponent } from './top/top.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BottomComponent,
+    TopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
